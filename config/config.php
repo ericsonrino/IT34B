e4ca2d2064036df590b3c56e49063799d26ca549
Francis Ericson <ericsonrino@gmail.com>
2026-09-13 14:14:01 +0800

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+require_once __DIR__ . '/../includes/activity-logger.php';
+
+define('BASE_URL', 'http://localhost/IT34B');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'it34b_lab_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+$user_id = "root" ?? null;
+$user_email = "root" ?? null;
+
+try {
+    $pdo = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4",
+        DB_USER,
+        DB_PASS,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ]
+    );
+
+    $success = logActivity(
+        $pdo,
+        $user_id,
+        $user_email,
+        'db_connect',
+        'success'
+    );
+
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
---
+++ db/schema.sql
@@ -0,0 +1,58 @@
+CREATE TABLE IF NOT EXISTS activity_logs (
+    activity_log_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(255),
+    user_email VARCHAR(255),
+    activity_log_action VARCHAR(50) NOT NULL,
+    activity_log_status ENUM('success', 'failed') DEFAULT 'success',
+
+    -- Client Parameters
+    activity_log_ip_address VARCHAR(45),
+    activity_log_user_agent VARCHAR(255),
+
+    -- Timestamps
+    activity_log_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table #3 users table
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+
+--Initial user details
+    user_email VARCHAR(50) UNIQUE NOT NULL,
+    user_username VARCHAR(20) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    user_role ENUM('admin', 'manager','user') NOT NULL DEFAULT 'user',
+
+    -- User Created Timestamps default not null
+
+    
+    
+);
+
+--insert query #1
+INSERT INTO users
+(
+    user_email,
+    user_username,
+    user_password,
+    user_role
+)
+VALUES
+(
+    'admin@example.com',
+    'admin',
+    '$2y$10$HNfhClczEWBxcFuJwP53iu2Y75Tba7IEtmX8vX.1tp0dZ5EVt9CbO',
+    'admin'
+),
+(
+    'manager@example.com',
+    'manager',
+    '$2y$10$HNfhClczEWBxcFuJwP53iu2Y75Tba7IEtmX8vX.1tp0dZ5EVt9CbO',
+    'manager'
+),
+(
+    'user@example.com',
+    'user',
+    '$2y$10$HNfhClczEWBxcFuJwP53iu2Y75Tba7IEtmX8vX.1tp0dZ5EVt9CbO',
+    'user'
+);
